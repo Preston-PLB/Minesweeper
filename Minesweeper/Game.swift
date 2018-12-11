8621be3cf673ab9b937869dab7a598fbe29cad41
Le, Brandon M <150116@s020rmc148-10.local>
2018-12-11 12:14:26 -0600

    added pseudo code to game logic for revealing surrounding blank tiles

--- Minesweeper/Game.swift
+++ Minesweeper/Game.swift
@@ -24,12 +24,47 @@ class Game
     }
     
     //TODO replace with button press logic
-//    @IBAction func tilePressed()
+    //    @IBAction func tilePressed(_ sender: UIButton)
 //    {
 //        if(!gameStarted)
 //        {
 //            startGame()
 //        }
-        //do button stuff
+        //button stuff pseudo code
+        //take the type of tile from button
+        //if mine, lose the game
+        //if clue, reveal the clue
+        //if blank, reveal all the touching blanks and clues.
+        //if flag option, flag the button, decrease number of mines remaining
+        //if no remaining mines, win the game
 //    }
+    
+    //TODO add func to reveal surrounding blank tiles if blank if tapped
+    //func revealBlanks(tile:Tile)
+    //{
+            //Take the tile tapped
+            //get all the surrounding tiles
+            //for each blank or clue surrounding tile, reveal it
+            //if(tile is revealed)
+            //{
+                //return
+            //}
+            //else if(tile is blank)
+            //{
+                //reveal this tile
+            //   for(each tile in surrounding tiles)
+                //{
+                    //if(tile is blank)
+                    //{
+                        //revealBlanks(tile)
+                    //}
+                    //else if(tile is clue)
+                    //{
+                        //reveal that tile
+                    //}
+                //}
+            //}
+    //}
+    
+    
 }
