fb707f61b185539362560078a8b43915e553d84f
Preston Baxter <preston@baxterboyz.com>
2018-12-11 12:12:37 -0600

    Started tile button UI element some more

--- Minesweeper/TileButton.swift
+++ Minesweeper/TileButton.swift
@@ -10,7 +10,21 @@ import Foundation
 import UIKit
 
 class TileButton: UIButton{
+
+    var tile: Tile?
     
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        self.layer.borderWidth = 2
+        self.layer.borderColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
+    }
     
+    func setTile(tile t: Tile){
+        tile = t
+    }
+    
+    //TODO: add click functions
     
 }
