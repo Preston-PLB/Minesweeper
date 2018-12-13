bbdce8230cfb1f0220f513d94e6fb39ecbfc948b
Le, Brandon M <150116@s020rmc148-10.local>
2018-12-13 11:07:47 -0600

    Finished up pseudo coding the game, need the interface to be finished to get it working

--- Minesweeper/Game.swift
+++ Minesweeper/Game.swift
@@ -11,6 +11,7 @@ import Foundation
 class Game
 {
     static var grid = Grid()
+    var gameStarted = false
     
     //Will create grids with different game settings
 //    init(settings:Settings)
@@ -18,53 +19,69 @@ class Game
 //
 //    }
     
+    //Called after first tile is tapped
     func startGame()
     {
         Game.grid.fillBoard()
     }
     
-    //TODO replace with button press logic
-    //    @IBAction func tilePressed(_ sender: UIButton)
+    //Called after restart button is tapped
+    func restart()
+    {
+        //hide all the tiles again
+        //reset timer and mine counter label
+        //gameStarted = false
+    }
+    
+    //PSEUDO CODE, Implement when storyboard stuff is ready
+    
+//    func checkTile(tile: Tile)
+//    {
+//        take the type of tile from button
+//        if mine, lose the game, make restart button appear
+//        if clue, reveal the clue
+//        if blank, revealBlanks(tile)
+//    }
+//
+//    TODO replace with button press logic
+//        @IBAction func tilePressed(_ sender: UIButton)
 //    {
 //        if(!gameStarted)
 //        {
 //            startGame()
 //        }
-        //button stuff pseudo code
-        //take the type of tile from button
-        //if mine, lose the game
-        //if clue, reveal the clue
-        //if blank, reveal all the touching blanks and clues.
-        //if flag option, flag the button, decrease number of mines remaining
-        //if no remaining mines, win the game
+//        button stuff pseudo code
+//        checkTile(sender)
+//
+//        if flag option, flag the button, decrease number of mines remaining
+//        if no remaining mines, win the game, bring up restart button
 //    }
-    
-    //TODO add func to reveal surrounding blank tiles if blank if tapped
-    //func revealBlanks(tile:Tile)
-    //{
-            //Take the tile tapped
-            //get all the surrounding tiles
-            //for each blank or clue surrounding tile, reveal it
-            //if(tile is revealed)
-            //{
-                //return
-            //}
-            //else if(tile is blank)
-            //{
-                //reveal this tile
-            //   for(each tile in surrounding tiles)
-                //{
-                    //if(tile is blank)
-                    //{
-                        //revealBlanks(tile)
-                    //}
-                    //else if(tile is clue)
-                    //{
-                        //reveal that tile
-                    //}
-                //}
-            //}
-    //}
+//
+//    TODO Connect to storyboard
+//    @IBAction func restartButton()
+//    {
+//        restart()
+//    }
+//
+//    TODO add func to reveal surrounding blank tiles if blank if tapped
+//    func revealBlanks(tile:Tile)
+//    {
+//            Take the tile tapped
+//            get all the surrounding tiles
+//            for each blank or clue surrounding tile, reveal it
+//            if(tile is revealed or a mine)
+//            {
+//                return
+//            }
+//            else if(tile is blank or a clue)
+//            {
+//                reveal this tile
+//               for(each tile in surrounding tiles)
+//                {
+//                    revealBlanks(tile)
+//                }
+//            }
+//    }
     
     
 }
