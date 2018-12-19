69e8e2f04e14c7bafedc4c6e4543a56230455891
Le, Brandon M <150116@s020rmc148-10.local>
2018-12-19 11:33:53 -0600

    commit my changes

--- Minesweeper/TileView.swift
+++ Minesweeper/TileView.swift
@@ -13,11 +13,13 @@ class TileView: UIView{
     
     var tile: Tile?
     
-    override init(frame: CGRect) {
+    override init(frame: CGRect)
+    {
         super.init(frame: frame)
     }
     
-    required init?(coder: NSCoder) {
+    required init?(coder: NSCoder)
+    {
         super.init(coder: coder)
     }
 }
